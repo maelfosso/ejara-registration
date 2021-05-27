cfbeac6808f08e6c76a23fa959d3594ed68a2821
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-05-27 14:28:52 +0100

    header is almost ok

--- lib/pages/register/view/register_form.dart
+++ lib/pages/register/view/register_form.dart
@@ -20,10 +20,11 @@ class RegisterForm extends StatelessWidget {
       },
       child: Container(
         color: Colors.white,
-        padding: EdgeInsets.symmetric(horizontal: 32.0),
         child: Column(
           children: [
-            _FormError(),
+            Expanded(
+              child: _buildHeader()
+            ),
             _buildBody(),
           ],
         )
@@ -31,19 +32,54 @@ class RegisterForm extends StatelessWidget {
     );
   }
 
+  _buildHeader() {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.only(bottom: 32.0),
+      decoration: BoxDecoration(
+        color: Colors.blue[900],
+        image: DecorationImage(
+          image: AssetImage("assets/images/ejara_bg.png"),
+          alignment: Alignment(1.0, -1.0)
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            child: Image(
+              image: AssetImage('assets/images/ejara_logo.png'),
+            )  
+          ),
+          Container(
+            child: Text(
+              "Create an account",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold
+              ),
+            )
+          )
+        ],
+      ),
+    );
+  }
 
   _buildBody() { 
     return Expanded(
       flex: 1,
       child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 32.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            _FormError(),
             _UsernameInput(),
             _EmailInput(),
             _PhoneNumberInput(),
             _RegisterButton()          
-            
           ],
         ),
       )
@@ -186,7 +222,7 @@ class _RegisterButton extends StatelessWidget {
                   child: Padding(
                     padding: EdgeInsets.symmetric(vertical: 15.0),
                     child: Text(
-                      "Sign Up",
+                      "Next",
                       textAlign: TextAlign.center,
                     )
                   ),
--- lib/pages/register/view/register_page.dart
+++ lib/pages/register/view/register_page.dart
@@ -11,15 +11,15 @@ class RegisterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.white,
+        // centerTitle: true,
+        backgroundColor: Colors.blue[900],
         elevation: 0.0,
-        title: Text(
-          "Register",
-          style: TextStyle(
-            color: Colors.black
-          ),
-        ),
+        // title: Text(
+        //   "Register",
+        //   style: TextStyle(
+        //     color: Colors.black
+        //   ),
+        // ),
         
       ),
       body: SafeArea( 
